0cdb2e8afb862cd7e5573a8a17e001b3b987bede
orpinelli <ga.f.orpinelli@gmail.com>
2025-06-23 14:59:23 -0300

    fix: ajuste deploy

--- app/dashboard/doacoes/page.tsx
+++ app/dashboard/doacoes/page.tsx
@@ -1,5 +1,10 @@
 import { DonationContainer } from "@/containers/Donation/donation.container";
+import { Suspense } from "react";
 
 export default function DoacoesPage() {
-  return <DonationContainer />;
+  return (
+    <Suspense>
+      <DonationContainer />;
+    </Suspense>
+  );
 }
